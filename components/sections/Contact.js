6fe3055ad214ee5e4576325cae879ef9e267ebf4
Ian Sui <ian.appdeture@gmail.com>
2022-07-05 19:48:21 +0800

    minor update

--- components/sections/Contact.js
+++ components/sections/Contact.js
@@ -45,11 +45,6 @@ export const Contact = ({ }) => {
   const addUser = () => {
     const requestOptions = {
       method: "POST",
-      headers: { 
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-        "Content-Type": "application/json"
-      },
       body: JSON.stringify({
         ...values
       }),
